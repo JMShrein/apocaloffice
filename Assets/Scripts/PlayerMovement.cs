62e52befaa7dfb8099cc208a867e37f40ed4e85e
John Shrein <jshrein@comcast.net>
2015-05-15 00:14:55 -0500

    Added headbob animation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,24 +2,24 @@
 using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
-
+	
 	public float RegSpeed = 5.0f;
 	public float sprintSpeed = 10.0f;
 	public float endurance = 3.0f;
-
+	
 	public CharacterController cc;
-
+	
 	public float mouseSensitivity = 5.0f;
 	float verticalRotation = 0;
 	public float upDownRange = 60.0f;
-
-
-
-
-
+	public GameObject camHolder;
+	
+	camAnimation animationScript;
+	
 	// Use this for initialization
 	void Start () 
 	{
+		animationScript = camHolder.GetComponent<camAnimation>();
 		Screen.lockCursor = true;
 	}
 	
@@ -28,52 +28,54 @@ public class PlayerMovement : MonoBehaviour {
 	{
 		print(endurance);
 		float speed = RegSpeed;
-
+		
 		endurance = Mathf.Clamp(endurance,0.0f,5.0f);
-	
-
-
+		
+		
+		
 		if (Input.GetKey(KeyCode.LeftShift))
 		{
+			animationScript.isRunning = true;
 			speed = sprintSpeed;
 			endurance -= Time.deltaTime;
-
-
-				if (endurance <= 0)
+			
+			
+			if (endurance <= 0)
 			{
 				speed = RegSpeed;
 			}
-
+			
 		}
 		else
 		{
+			animationScript.isRunning = false;
 			endurance += Time.deltaTime;
 		}
 		
 		
-
+		
 		float forward = Input.GetAxis("Horizontal") * speed;
 		float sideways = Input.GetAxis("Vertical") * speed;
-
+		
 		Vector3 movement = new Vector3(forward, 0.0f, sideways);
-
+		
 		movement = transform.rotation * movement;
-
+		
 		cc = GetComponent<CharacterController>();
-
+		
 		cc.SimpleMove(movement);
-
-
+		
+		
 		float rotLeftRight = Input.GetAxis("Mouse X") * mouseSensitivity;
 		transform.Rotate(0, rotLeftRight, 0);
-
-
+		
+		
 		verticalRotation -= Input.GetAxis("Mouse Y") * mouseSensitivity;
 		verticalRotation = Mathf.Clamp(verticalRotation, -upDownRange, upDownRange);
 		Camera.main.transform.localRotation = Quaternion.Euler(verticalRotation, 0, 0);
-	
-
-
-
+		
+		
+		
+		
 	}
 }
---
+++ Assets/Scripts/camAnimation.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class camAnimation : MonoBehaviour {
+
+	public CharacterController playerController;
+	public Animation anim;
+	private bool isMoving;
+	public bool isRunning;
+
+	private bool left;
+	private bool right;
+
+	void CamAnimations() {
+		if(isRunning)
+		{
+			if(playerController.isGrounded == true) {
+				if(isMoving == true) {
+					if(left == true) {
+						if(!anim.isPlaying){
+							anim.Play ("runLeft");
+							left = false;
+							right = true;
+						}
+					}
+					if(right == true)
+					{
+						if(!anim.isPlaying) {
+							anim.Play ("runRight");
+							right = false;
+							left = true;
+						}
+					}
+				}
+			}
+		}
+		else
+		{
+			if(playerController.isGrounded == true) {
+				if(isMoving == true) {
+					if(left == true) {
+						if(!anim.isPlaying){
+							anim.Play ("walkLeft");
+							left = false;
+							right = true;
+						}
+					}
+					if(right == true)
+					{
+						if(!anim.isPlaying) {
+							anim.Play ("walkRight");
+							right = false;
+							left = true;
+						}
+					}
+				}
+			}
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		left = true;
+		right = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float inputX = Input.GetAxis("Horizontal");
+		float inputY = Input.GetAxis("Vertical");
+
+		if(inputX != 0 || inputY != 0) {
+			isMoving = true;
+		}
+		else if(inputX == 0 && inputY == 0) {
+			isMoving = false;
+		}
+
+		CamAnimations();
+	}
+}
