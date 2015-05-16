643ec210621bac8d9c0e8faaeb2118cd29019ef2
Morgan <nihuspawns@gmail.com>
2015-05-15 23:15:44 -0400

    More of Buzz's stuff

---
+++ Assets/Scripts/phoneController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class phoneController : MonoBehaviour {
+
+	public Animation anim;
+	private bool isOn = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		handleKeyPress();
+	}
+
+	void handleKeyPress() {
+		print ("Handle Key Press");
+		if(Input.GetKey(KeyCode.F)) {
+			print("F key pressed");
+			if(isOn) {
+				anim.Play("phoneDown");
+			}
+			else {
+				anim.Play("phoneUp");
+			}
+			isOn = !isOn;
+		}
+	}
+}
